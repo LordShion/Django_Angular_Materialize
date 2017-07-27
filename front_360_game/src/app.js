5edb5c793c0f5929be9b8f4159ff6c8cc5f08948
fsilva <fsilva@aconex.com>
2017-07-28 00:31:55 +0200

    wip angular boot

--- front_360_game/src/app.js
+++ front_360_game/src/app.js
@@ -1,2 +1,12 @@
-cats = require('./cats.js');
-console.log(cats);
+//cats = require('./cats.js');
+import angular from 'angular';
+import boot_website from './boot_website.js';
+//console.log(cats);
+
+const app = angular.module( 'app', [
+	boot_website
+] )
+	.name;
+
+
+export default app;
